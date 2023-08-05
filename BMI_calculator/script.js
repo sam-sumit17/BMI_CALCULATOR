bc4df99cc69fd739fa11db6b5e2ef5fee1622b0c
sam-sumit17 <sumit00022171@gmail.com>
2023-08-06 01:07:42 +0530

    BMI LATEST

--- BMI_calculator/script.js
+++ BMI_calculator/script.js
@@ -6,19 +6,19 @@ function sam(){
     
    h=document.getElementById('height').value;
    w=document.getElementById('weight').value;
-   if(h===""||h===0)alert("Height daal bsdk");
-   if(w===""||w===0)alert("Weight daal bsdk");
-   if(h>2.5&&h>0)alert("Aadmi ho ki uut(camel) ho bc 🐪");
-   if(w>100&&w>0)alert("sharm kar bhadwe ......hathi kahika 🐘")
+   if(h===""||h===0)alert("Please Enter your height");
+   if(w===""||w===0)alert("Please Enter your weight");
+   if(h>2.5&&h>0)alert("Enter a valid height");
+   if(w>100&&w>0)alert("Enter a valid weight")
 
     h*=h; 
     bmi=((w/h)/0.01)*0.01;
      y=document.getElementById('names').value;
     if(document.getElementById('gender').value==='Female'){x='Miss.';}
     else x='Mr.';
+ document.getElementById('suggestion').innerHTML='According to ideal BMI range, your weight should be in between '+ (18.5*h).toFixed(1) + 'kg and '+ (25*h).toFixed(1) +'kg. ';
    
-   
-    document.getElementById('bmi_ans').innerHTML=x+" "+y+ " " +bmi+ " is your BMI (Body Mass Index).";
+    document.getElementById('bmi_ans').innerHTML=x+" "+y+ " " +bmi.toFixed(1)+ " is your BMI (Body Mass Index).";
     if(bmi<=18.5){
         document.getElementById('bmi_colour').style.backgroundColor='yellow';
         document.getElementById('bmi_colour').style.color='black';
